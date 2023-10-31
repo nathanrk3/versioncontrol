43e7aa96830301932f818dcb91132445db92a9ca
unknown <igawam@ufl.edu>
2023-10-31 16:39:31 -0400

    Decode

--- versioncontrol.py
+++ versioncontrol.py
@@ -10,6 +10,14 @@ def encode_password(user_input):
     return empty_string
 
 
+# Manato Igawa
+# decode the encoded password
+def decode(data):
+    encoded_list = [int(digit) for digit in data]
+    decoded_list = [(digit + 7) % 10 for digit in encoded_list]
+    decoded_pw = ''.join(map(str, decoded_list))
+    return decoded_pw
+
 
 def main():
 
@@ -20,18 +28,27 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
+        print()  # Manato Igawa
 
         # make input
-        user_choice = input("Please enter an option:")
+        user_choice = input("Please enter an option: ")
         if user_choice == '1':
-            non_coded = input("Please enter your password to encode:")
+            non_coded = input("Please enter your password to encode: ")
 
-            #encode the password
+            # encode the password
             password = encode_password(non_coded)
             print("Your password has been encoded and stored!")
+            print()  # Manato Igawa
+
+        #  Manato Igawa
+        elif user_choice == '2':
+            # decode the encoded password
+            print(f'The encoded password is {encode_password(non_coded)}, and the original password is {decode(password)}.')
+            print()
 
         elif user_choice == '3':
             break
+
 
 if __name__ == "__main__":
     main()
